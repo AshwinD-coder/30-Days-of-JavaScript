c318a67c8e9fa53e05e44962a1f8936564cb7611
AshwinD-Coder <ashwindg15@gmail.com>
2023-04-28 19:02:29 +0545

    Day 1 Completed

---
+++ Day1_exercises/datatypes.js
@@ -0,0 +1,8 @@
+let age = 22
+let firstletter = 'A'
+let firstname = `Ashwin`
+let weight = 66.74
+let isMotivated = true
+let futureBody 
+let sixpacks = null
+console.log("5-> ",typeof(age),typeof(firstletter),typeof(firstname),typeof(weight),typeof(isMotivated),typeof(futureBody),typeof(sixpacks))
---
+++ Day1_exercises/variables.js
@@ -0,0 +1,5 @@
+let string_name = "Ashwin" //String type
+let isProgrammer = true
+let successful
+let skill  = null
+console.log("4->",string_name,isProgrammer,successful,skill)
